db35a79a2f86a6aeaf9677140c1f6bd1b7810b6d
yaakoubjoumail <yaakoubjou@gmail.com>
2023-06-20 18:50:23 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,21 @@
 /**
  * main - print _putchar
  * is written by mbah
- * return: 0
+ * Return: 0
  */
+
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
+
